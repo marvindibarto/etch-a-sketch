92ec70459ea3053bb63cf997f12464dad3d5ab17
marvindibarto <marvin.dibartolomeo@gmail.com>
2024-07-04 01:46:13 +0200

    Button on top of the site triggers a prompt and asks for input which is used to create a new etch-a-sketch

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //variable to initialize size of squares for when website is visited
 let size = "16px"
+let amount = 5;
 //i think this is actually irrelevant
     let width = window.innerWidth;
     let height = window.innerHeight;
@@ -17,40 +18,92 @@ div.style.border = 0;
 div.style.display = "flex";
 div.style.flexWrap = "wrap";
 
+//create and style button on top of page
+const buttonDiv = document.createElement("div");
+buttonDiv.style.width = "100%";
+buttonDiv.style.height = "20px";
+//class needed for distinction to square elements when formatting
+buttonDiv.classList.add("btn");
 
-produceSquares(1000);
+const inputButton = document.createElement("button");
+inputButton.textContent = "Change number of squares";
+inputButton.addEventListener("click", getInput)
+
+div.appendChild(buttonDiv);
+buttonDiv.appendChild(inputButton);
+
+//we need an extra div to later delete all children but NOT the button
+const squareDiv = document.createElement("div");
+//style div of container of squares
+squareDiv.style.margin = 0;
+squareDiv.style.padding = 0;
+squareDiv.style.border = 0;
+squareDiv.style.display = "flex";
+squareDiv.style.flexWrap = "wrap";
+div.appendChild(squareDiv);
+
+
+
+
+//call function to produce the wanted amount of squares
+produceSquares(amount);
+
+
+//all needed functions
+function getInput(){
+    amount = prompt("Please enter the number of squares to be created:","Enter number");
+    removeChildren();
+    produceSquares(amount);
+    // setStartProperties();
+}
 
 //create function to produce an amount of squares and append them to the div
 function produceSquares(amount){
     let squares = [];
     for (let i = 0; i < amount; i++){
         squares.push(document.createElement("div"));
-        div.appendChild(squares[i]);
+        setStartProperties(squares[i]);
+        //this was outsourced to function setStartProperties
+        // squares[i].style.margin = 0;
+        // squares[i].style.padding = 0;
+        // squares[i].style.border = 0;
+        // squares[i].style.height = size;
+        // squares[i].style.width = size;
+        // squares[i].style.flexGrow = 0;
+        // squares[i].style.flexShrink = 0;
+        // squares[i].style.backgroundColor = "blue";
+        // squares[i].addEventListener("mouseenter",(event)=>{
+        //     event.target.style.backgroundColor = "red";
+        // })
+        squareDiv.appendChild(squares[i]);
     }
 }
 
-//give no boarder etc. to all children inside the div
-for (const child of div.children){
-    console.log(child);
-    child.style.margin = 0;
-    child.style.padding = 0;
-    child.style.border = 0;
-    child.style.height = size;
-    child.style.width = size;
-    child.style.flexGrow = 0;
-    child.style.flexShrink = 0;
-    child.style.backgroundColor = "blue";
+function setStartProperties(child){
+    //give no boarder etc. to all children inside the div which are not the btn-div
+    for (children in squareDiv){
+            child.style.margin = 0;
+            child.style.padding = 0;
+            child.style.border = 0;
+            child.style.height = size;
+            child.style.width = size;
+            child.style.flexGrow = 0;
+            child.style.flexShrink = 0;
+            child.style.backgroundColor = "blue";
 
-    //this is one way to make the background color change when the mouse enters the dedicated square
-    // child.addEventListener("mouseenter",(event)=>{
-    //     event.target.style.backgroundColor = "red";
-    // })
+            //this is one way to make the background color change when the mouse enters the dedicated square
+            child.addEventListener("mouseenter",(event)=>{
+                event.target.style.backgroundColor = "red";
+            })
+    
 
-    //this is the other way and completely interchangable but needs a style.css sheet (see there for more infos)
-    child.addEventListener("mouseenter",(event)=>{
-        event.target.classList.add("hovered");
-    })
+            //this is the other way and completely interchangable but needs a style.css sheet (see there for more infos)
+            // child.addEventListener("mouseenter",(event)=>{
+            //     event.target.classList.add("hovered");
+            // })
+    }
 }
 
-
-
+function removeChildren(){
+    squareDiv.replaceChildren();
+}
